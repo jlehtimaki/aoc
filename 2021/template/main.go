9a551e2280cdf606cbf83bad5a9c34567d9e9dd6
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2021-12-07 09:25:00 +0200

    added template

---
+++ 2021/template/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readLines(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	defer file.Close()
+
+	var stringArray []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		stringArray = append(stringArray, scanner.Text())
+	}
+	return stringArray
+}
+
+func solveProblem(input string) int {
+	var count int
+	return count
+}
+
+func main()  {
+	fmt.Println(solveProblem("input.txt"))
+}
